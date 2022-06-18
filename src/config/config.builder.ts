e48d4bdec5c36908cf78955844f61f5b2d64a070
Microwavekonijn <microwavekonijn@pm.me>
2022-06-18 23:50:59 +0200

    fix: config builder create instance not provider

--- src/config/config.builder.ts
+++ src/config/config.builder.ts
@@ -1,37 +1,34 @@
-import { FactoryProvider, Logger, Type } from '@nestjs/common';
+import { Logger, Type } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { validateOrReject } from 'class-validator';
 import { configStorage } from './config.storage';
 
-export function configBuilder(config: Type): FactoryProvider {
-  return {
-    provide: config,
-    useFactory: async () => {
-      const logger = new Logger('Config');
-      const base = {} as any;
+export async function configBuilder<T extends object>(
+  config: Type<T>,
+): Promise<T> {
+  const logger = new Logger('Config');
+  const base = {} as any;
 
-      // Hydrate the base instance
-      for (const property of configStorage.getEnvProperties(config)) {
-        const options = configStorage.getEnvMetadata(config, property);
+  // Hydrate the base instance
+  for (const property of configStorage.getEnvProperties(config)) {
+    const options = configStorage.getEnvMetadata(config, property);
 
-        if (options && process.env[options.env])
-          base[property] = process.env[options.env];
-      }
+    if (options && process.env[options.env])
+      base[property] = process.env[options.env];
+  }
 
-      // Transform the base instance
-      const instance = plainToInstance(config, base, {
-        excludeExtraneousValues: true,
-      });
+  // Transform the base instance
+  const instance = plainToInstance(config, base, {
+    excludeExtraneousValues: true,
+  });
 
-      try {
-        // Validate the values
-        await validateOrReject(instance);
-      } catch (errors) {
-        logger.error('Invalid configuration', errors);
-        process.exit(1);
-      }
+  try {
+    // Validate the values
+    await validateOrReject(instance);
+  } catch (errors) {
+    logger.error('Invalid configuration', errors);
+    process.exit(1);
+  }
 
-      return Object.freeze(instance);
-    },
-  };
+  return Object.freeze(instance);
 }
